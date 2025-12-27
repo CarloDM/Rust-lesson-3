b27a3b60083aadb27817deb6b62506c9cd5ed8d3
CarloDM <carlo.de.mauro.work@gmail.com>
2025-12-27 16:58:59 +0100

    clean not usefull pub

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-pub mod modulo_test ;
+mod modulo_test ;
 // use crate::modulo_test::modulo_test2::Prova ;
-pub use crate::modulo_test::Prova ;
+use crate::modulo_test::Prova ;
 
 fn main() {
     let test_a = Prova{
--- src/modulo_test.rs
+++ src/modulo_test.rs
@@ -1,4 +1,4 @@
-pub mod modulo_test2 ;
+mod modulo_test2 ;
 
 // aggiungendo questo, da main possiamo saltare i moduli intermedi ed arrivare direttamente a use
 pub use modulo_test2::Prova;
